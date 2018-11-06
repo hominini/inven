d6ceea7f384392dc8c96317fc16ed0fc2b4decda
simian <byronrpv_rckr@hotmail.com>
2018-11-06 13:46:36 -0500

    Renombrada tabla de bienes_de_control.. a bienes

--- database/migrations/2018_11_05_173308_crear_tabla_bienes.php
+++ database/migrations/2018_11_05_173308_crear_tabla_bienes.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CrearTablaBienesDeControlAdministrativo extends Migration
+class CrearTablaBienes extends Migration
 {
     /**
      * Run the migrations.
