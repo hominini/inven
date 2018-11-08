306c7f45aae950ad4e2dccc2e32b610259dbf445
simian <byronrpv_rckr@hotmail.com>
2018-11-08 13:37:38 -0500

    Agregada migracion items bibliograficos

---
+++ database/migrations/2018_11_08_183238_crear_tabla_items_bibliograficos.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaItemsBibliograficos extends Migration
+{
+    public function up()
+    {
+        Schema::create('items_bibliograficos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_bien');
+            $table->integer('id_tipo_de_bien');
+            $table->string('autor')->nullable();
+            $table->string('detalles_de_publicacion')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('items_bibliograficos');
+    }
+}
