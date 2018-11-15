652a0b8b32d31b50913441134d33cd8d3621f0d7
simian <byronrpv_rckr@hotmail.com>
2018-11-15 16:34:50 -0500

    Comentado

--- public/index.php
+++ public/index.php
@@ -55,6 +55,6 @@ $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
 
-$response->send();
+$response->send(); // send the final response to the browser
 
 $kernel->terminate($request, $response);
