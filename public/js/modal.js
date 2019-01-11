c26b549dd8b20983d387228f2a8f9996039caef4
superadmin <bvega@localhost.localdomain>
2019-01-11 22:16:22 +0000

    Ingresando ubicaciones con ajax

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,51 +1,64 @@
-// En este archivo poner todos los metodos que manejan submits desde ventanas
-// modales
-$.ajaxSetup({
-    headers: {
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-    }
-});
 
 (function() {
+    // El ambito del request debe ser local, para evitar conflictos
     var httpRequest;
-    document.getElementById('modalUbicacion').addEventListener('click', makeRequest);
 
+    // Se registra el metodo que manejara el submit de datos en el formulario modal
+    document.getElementById('modalUbicacion').addEventListener('click', almacenarUbicacion);
 
-    function makeRequest() {
+    function almacenarUbicacion() {
 
+        // Se obtiene los datos que se van a almacenar en la bdd
         var nombre = document.getElementById('nombre_ubicacion').value;
         var nombre_cuarto = document.getElementById('nombre_cuarto').value;
         var comentarios = document.getElementById('comentarios').value;
-        var csrf_token = document.getElementById('csrf_token').content;
+        var csrf_token = document.head.querySelector('meta[name="csrf-token"]').content;
 
+        // Se crea una instancia de un objeto xhr
         httpRequest = new XMLHttpRequest();
 
+        // Se comprueba la creacion del objeto xhr, se aborta el proceso en caso de falla
         if (!httpRequest) {
           alert('Abortando :( No se puede crear una instancia XMLHttpRequest');
           return false;
         }
 
-        httpRequest.onreadystatechange = alertContents;
+        // Se agrega el nuevo registro a la pagina en caso de respuesta exitosa
+        httpRequest.addEventListener('load', function(event) {
+            refrescarUbicaciones(httpRequest.response)
+        });
+
+        // Se imprime en la consola la causa del error en dicho caso
+        httpRequest.addEventListener('error', function(event) {
+            console.log('Error al crear el recurso.');
+        });
+
         httpRequest.open('POST', '/ubicaciones');
+        httpRequest.responseType = 'json';
+        httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+        // Se envia la peticion
         httpRequest.send(
           "_token=" + csrf_token +
-          "&nombre=" + nombre +
-          "&nombre_cuarto=" + nombre_cuarto +
-          "&comentarios=" + comentarios
+          "&nombre=" + encodeURIComponent(nombre) +
+          "&nombre_cuarto=" + encodeURIComponent(nombre_cuarto) +
+          "&comentarios=" + encodeURIComponent(comentarios)
         );
-        alert(httpRequest.status);
+
     }
 
-    function alertContents() {
+    function refrescarUbicaciones(ubicacion) {
 
-        if (httpRequest.readyState === XMLHttpRequest.DONE) {
-            if (httpRequest.status === 200) {
-                alert(httpRequest.responseText);
-            } else {
-                alert('There was a problem with the request.');
-              }
-          }
+        var select = document.getElementById('id_ubicacion');
+
+        var opcion = document.createElement("option");
+
+        opcion.text = ubicacion.nombre;
+        opcion.values = ubicacion.id;
+        opcion.setAttribute('selected', true);
+
+        select.add(opcion);
+
     }
-}
 
-)()
+})();
