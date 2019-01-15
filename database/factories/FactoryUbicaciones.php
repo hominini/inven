a678b8c615f538a880a548dfcd88eabb53518f87
hominini <byronrpv_rckr@hotmail.com>
2019-01-15 20:24:08 +0000

    Cambio minimo

--- database/factories/FactoryUbicaciones.php
+++ database/factories/FactoryUbicaciones.php
@@ -4,8 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Ubicacion::class, function (Faker $faker) {
     return [
-        'nombre' => $faker->word,
-        'comentarios' => $faker->paragraph,
-        'nombre_cuarto' => $faker->sentence,
+      'nombre' => $faker->word,
+      'comentarios' => $faker->paragraph,
+      'nombre_cuarto' => $faker->name,
     ];
 });
