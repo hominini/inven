422bc50d1a4cbec1808fcf00479544a33a5bd40c
simian <byronrpv_rckr@hotmail.com>
2019-06-20 01:30:12 -0500

    Agregadas rutas en api para muebles

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,37 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('muebles', function() {
+
+    $muebles = \App\Mueble::all();
+    foreach ($muebles as $mueble) {
+        $mueble->bien_control_administrativo;
+        $mueble->bien_control_administrativo->bien;
+    }
+
+    return $muebles->toJSON(JSON_PRETTY_PRINT);
+});
+
+Route::get('muebles/{id}', function($id) {
+
+    $mueble = \App\Mueble::find($id);
+    $mueble->bien_control_administrativo;
+    $mueble->bien_control_administrativo->bien;
+    $muebleReturn = $mueble->toArray();
+   
+    foreach ($muebleReturn['bien_control_administrativo'] as $key => $val)
+    {   
+        $muebleReturn[$key] = $val;
+    }
+
+    foreach ($muebleReturn['bien_control_administrativo']['bien'] as $key => $val)
+    {   
+        $muebleReturn[$key] = $val;
+    }
+
+    unset($muebleReturn['bien_control_administrativo']);
+    unset($muebleReturn['bien']);
+    
+    return json_encode($muebleReturn);
+});
