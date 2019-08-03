79d80959b090d39835f5fb0fadc0bf57d378d400
simian <byronrpv_rckr@hotmail.com>
2019-08-03 09:33:49 -0500

    agregado modulo de control de api

---
+++ tests/Feature/PruebasBienesAPITest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Artisan;;
+
+class PruebasBienesAPI extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp()
+    {
+        parent::setUp();
+        //Artisan::call('migrate');
+        Artisan::call('db:seed');
+    }
+    /**
+     * @test
+     */
+    use RefreshDatabase;
+
+    public function por_medio_de_la_api_se_obtienen_todos_los_bienes()
+    {
+        // preparar data'
+        $bienes = \App\Bien::all();
+
+        // intentar petición
+        $respuesta = $this->json('GET', '/api/bienes');
+
+        // valorar el resultado
+        $respuesta
+            ->assertStatus(201)
+            ->assertJson([
+                // 'bienes' =
+            ]);
+    }
+
+
+}
