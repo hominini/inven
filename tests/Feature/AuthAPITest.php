982be6dcdb08dbe9fd4f1fd2b762c9986405a1ad
simian <byronrpv_rckr@hotmail.com>
2019-08-04 18:39:48 -0500

    agregado  test para autorizacion por medio de api

---
+++ tests/Feature/AuthAPITest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Artisan;;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AuthAPITest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp()
+    {
+        parent::setUp();
+        Artisan::call('migrate');
+        Artisan::call('db:seed');
+    }
+
+    /** @test */
+    public function usuario_se_puede_registrar_atraves_de_la_api()
+    {
+        $this->withoutExceptionHandling();
+        // preparar data
+        $data2 = [
+            'form-params' => [
+                'name' => 'admin@admin.com',
+                'email' => 'admin@admin.com',
+                'password' => 'password',
+                'c_password' => 'password',
+            ],
+        ];
+        $data = [
+            'name' => 'admin@admin.com',
+            'email' => 'admin@admin.com',
+            'password' => 'password',
+            'c_password' => 'password',
+        ];
+
+        // intentar la petición
+        $respuesta = $this->json('POST', '/api/register', $data);
+
+        $respuesta->dump();
+        // valorar el resultado
+        $respuesta->assertStatus(201);
+    }
+}
