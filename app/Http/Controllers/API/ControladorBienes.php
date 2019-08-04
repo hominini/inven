edb7bcf12b439c3e36cca522366a65e1e6ca44a9
simian <byronrpv_rckr@hotmail.com>
2019-08-04 18:38:32 -0500

    controladores para api

---
+++ app/Http/Controllers/API/ControladorBienes.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Product;
+use Validator;
+
+class ControladorBienes extends Controller
+{
+
+}
