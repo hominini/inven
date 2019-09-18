5b00b7ebeac4a4bf4e3ed83bafd3da8b3df809df
simian <byronrpv_rckr@hotmail.com>
2019-09-18 02:51:08 -0500

    trabajando en evento para eliminar tokens viejos

---
+++ app/Listeners/RevokeOldTokens.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Listeners;
+
+use Laravel\Passport\Events\AccessTokenCreated;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+use Laravel\Passport\Client;
+use Carbon\Carbon;
+
+class RevokeOldTokens
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  AccessTokenCreated  $event
+     * @return void
+     */
+    public function handle(AccessTokenCreated $event)
+    {
+        $client = Client::find($event->clientId);
+        $client->tokens()->where('created_at', '<', Carbon::now()->subDay())->forcedelete();
+
+    }
+}
