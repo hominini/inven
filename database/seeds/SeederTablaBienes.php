8a926c82fbd5ecf0c14222cc9c2b40f089e6c23c
simian <byronrpv_rckr@hotmail.com>
2019-09-18 03:46:39 -0500

    actualizado seeder bienes

--- database/seeds/SeederTablaBienes.php
+++ database/seeds/SeederTablaBienes.php
@@ -11,6 +11,20 @@ class SeederTablaBienes extends Seeder
      */
     public function run()
     {
-        $bienes = factory(App\Bien::class, 50)->create();
+        factory(App\Bien::class, 5)->create([
+            'id_ubicacion' => 1,
+        ]);
+
+        factory(App\Bien::class, 4)->create([
+            'id_ubicacion' => 2,
+        ]);
+
+        factory(App\Bien::class, 5)->create([
+            'id_ubicacion' => 3,
+        ]);
+
+        factory(App\Bien::class, 1)->create([
+            'id_ubicacion' => 4,
+        ]);
     }
 }
